901d3b160b06f9e44b93587158cfa4380874bb83
cseMisbah <mijbahsakib@gmial.com>
2025-02-18 19:36:14 +0600

    developed product listing app

--- PRODUCT-LISTING-APP/src/App.jsx
+++ PRODUCT-LISTING-APP/src/App.jsx
@@ -1,35 +1,150 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const products = [
+    {
+      id: 1,
+      title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+      price: 109.95,
+      description:
+        "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+      category: "men's clothing",
+      image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+      rating: {
+        rate: 3.9,
+        count: 120,
+      },
+    },
+    {
+      id: 2,
+      title: "Mens Casual Premium Slim Fit T-Shirts ",
+      price: 22.3,
+      description:
+        "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
+      category: "men's clothing",
+      image:
+        "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+      rating: {
+        rate: 4.1,
+        count: 259,
+      },
+    },
+    {
+      id: 3,
+      title: "Mens Cotton Jacket",
+      price: 55.99,
+      description:
+        "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
+      category: "men's clothing",
+      image: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
+      rating: {
+        rate: 4.7,
+        count: 500,
+      },
+    },
+    {
+      id: 4,
+      title: "Mens Casual Slim Fit",
+      price: 15.99,
+      description:
+        "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
+      category: "men's clothing",
+      image: "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
+      rating: {
+        rate: 2.1,
+        count: 430,
+      },
+    },
+    {
+      id: 5,
+      title:
+        "John Hardy Women's Legends Naga Gold & Silver Dragon Station Chain Bracelet",
+      price: 695,
+      description:
+        "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
+      category: "jewelery",
+      image: "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg",
+      rating: {
+        rate: 4.6,
+        count: 400,
+      },
+    },
+    {
+      id: 6,
+      title: "Solid Gold Petite Micropave ",
+      price: 168,
+      description:
+        "Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days.",
+      category: "jewelery",
+      image: "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg",
+      rating: {
+        rate: 3.9,
+        count: 70,
+      },
+    },
+  ];
 
   return (
-    <>
+    <div className="container">
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1 className="headingStyle">Bd Store</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div
+        style={{
+          display: "grid",
+
+          gridTemplateColumns: "auto auto auto",
+          gridGap: "10px",
+          //justifyContent: "space-around",
+        }}
+      >
+        {products.map((item, index) => (
+          <div
+            key={item.id}
+            style={{
+              border: "1px solid #ddd",
+              padding: "16px",
+              borderRadius: "8px",
+
+              boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+            }}
+          >
+            <img src={item.image} alt="" height="200px" width="200" />
+            <h3 className="titleText">{item.title}</h3>
+
+            <p
+              className="pRate"
+              style={{ color: "red", fontSize: "15px", fontWeight: "bold" }}
+            >
+              Rating : {item.rating.rate}
+            </p>
+            <p
+              className="pCount"
+              style={{ color: "red", fontSize: "15px", fontWeight: "bold" }}
+            >
+              Count : {item.rating.count}
+            </p>
+            <p style={{ color: "white", fontSize: "8px", fontWeight: "bold" }}>
+              {item.description}
+            </p>
+            <button
+              className="myBtn"
+              style={{ color: "white", fontSize: "12px" }}
+            >
+              Add to cart
+            </button>
+          </div>
+        ))}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
+
+/*
+<div className="mainDiv">
+      <h1 className="headingStyle">Bd Store</h1>
+      <Products products={products}></Products>
+    </div>
+*/
